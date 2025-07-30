171b612d4a385ddadafd31a0070841099013c9c2
IvanThoux <thouxivan4@gmail.com>
2025-07-30 13:10:37 -0300

    Primer commit: creacion de los archivos

---
+++ unidadI.py
@@ -0,0 +1,68 @@
+'''Esta es la unidad 1 del camino a la programacion'''
+'''
+/* UNIDAD I:
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+
+'''
+# Pagina oficial de python: https://www.python.org
+
+# Para hacer comentarios simples de una sola linea se utiliza en numeral '#'.
+# Para hacer comentarios de multiples lineas ''' Comentario multiples lineas ''' se utilizan tres comillas simples.
+# Este tipo de comentario de varias lineas tambien se utiliza como docstring, para comentar funciones, clases, etc.
+
+# Para las variables se utiliza snake_case
+nombre_completo = "Ivan Ezequiel Thoux"
+
+# Para las constantes se recomiendo escribir el nombre en mayusculas
+VALOR_PI = 3.1416
+
+# Para las clases se recomienda utilizar CamelCase
+
+''' 
+Tipos de datos en python:
+
+# Dato de tipo entero
+EDAD = 25
+NUMERO_NEGATIVO = -10
+
+# Dato de tipo float
+altura = 1.75
+temperatura_promedio = 28.4
+
+# Dato de tipo booleano
+aparato_encendido = True
+aparato_apagado = False
+
+# Dato de tipo string o cadena
+MENSAJE = "Hola mundo!"
+SALUDO = "Buenos dias"
+
+# Dato de tipo nulo o sin valor
+resultado = None
+
+Existe la funcion type(constante/variable) que nos permite determinar el
+tipo de dato de una constante/variable.
+'''
+
+# Imprimimos un saludo para finalizar la consigna:
+print("Hola mundo.")
